31eaec83b01d36fc9e5d507f7aa12a0992bde01f
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-02-06 22:26:52 +0000

    Ready for Grading!

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,21 +1,43 @@
 #
-# Name
-# Date
-# Areas of Rectangles Programming Project
+# Name: Wil Wagner
+# Date: 4 Feb 2025
+# Areas of Rectangles
 # COSC 1010
 #
-# Local variables
 
-# Get length A
+# Local Variables
+length1 = 0     # Length of rectangle 1
+width1 = 0      # Width of rectangle 1
+length2 = 0     # Length of rectangle 2
+width2 = 0      # Width of rectangle 2
+area1 = 0       # Area of rectangle 1
+area2 = 0       # Area of rectangle 2
 
-# Get width A
+# Get the length of rectangle 1.
+length1 = int(input('Enter the length of rectangle 1: '))
 
-# Get length B
+# Get the width of rectangle 1.
+width1 = int(input('Enter the width of Rectangle 1: '))
 
-# Get width B
+# Get the length of rectangle 2.
+length2 = int(input('\nEnter the length of rectangle 2: '))
 
-# Calculate area A
+# Get the width of rectangle 2.
+width2 = int(input('Enter the width of Rectangle 2: '))
 
-# Calculate area B
+# Calculate the area of rectangles 1.
+area1 = length1 * width1
 
-# Print area comparison using if-elif-else statements
+# Calculate the area of rectangle 2.
+area2 = length2 * width2
+
+# Determine which has the greater area.
+if area1 > area2:
+    # Print that rectangle 1 has the greatest area.
+    print('Rectagle 1 has the greater area.')
+elif area2 > area1:
+    # Print that rectangle 2 has the greatest area.
+    print('Rectangle 2 has the greater area.')
+else:
+    # Print that both rectangles are the same.
+    print('Both have the same area.')
