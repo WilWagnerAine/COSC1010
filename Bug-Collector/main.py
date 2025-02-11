eb4b7d5c234346a5fbc92feeddbdcfc6969e0421
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-02-11 20:05:34 +0000

    Ready for Grading!

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,11 +1,25 @@
 #
-# Name
-# Date
-# Bug Collector Programming Project
+# Name: Wil Wagner
+# Date: 2/11/2025
+# Bug Collector
 # COSC 1010
 #
-# Initialize variables for bugs and total number of bugs collected.
 
-# Get number of bugs collected each day using a for loop.
+# Set accumulator (total) to 0
+total = 0
 
-# Display the total number of bugs collected.
+# Set day to 0
+day = 0
+
+# Set bugs to 0
+bugs = 0
+
+# For each of 7 days:
+for day in range(1, 8):
+    # Get number of bugs collected that day.
+    bugs = int(input(f'How many bugs did you collect day {day}: '))
+    # Add bugs collected to the total
+    total += bugs
+
+# Print total amount of bugs
+print(f'\nYou have collected a grand total of {total} bugs')
