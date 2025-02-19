dd980d17954fb7e9fb0dc40c53b35811d7730b72
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-02-19 21:44:55 +0000

    Ready for Grading!

--- Kilometer-Converter/main.py
+++ Kilometer-Converter/main.py
@@ -1,7 +1,35 @@
 #
-# Name
-# Date
-# Kilometer Converter Programming Project
+# Name: Wil Wagner
+# Date:
+# Kilometer Converter problem
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+# Named Constant
+CONVERSION_FACTOR = 0.6214
+
+# Declare variables
+# No Declared variables due to the variables being local.
+
+# The main function gets a distance in kilometers and calls
+# the show_miles function to convert it.
+def main():
+    kilometers = 0          # Set the local initial value of kilometers to 0.
+    # Get the distance in kilometers.
+    kilometers = float(input('Enter a distance in kilometers: '))
+
+    # Display the distance coverted to miles.
+    show_miles(kilometers)
+
+# The show_miles function converts the parameter km from
+# kilometers to miles and displays the results.
+def show_miles(km):
+    miles = 0               # Set the local initial value of miles to 0.
+    # Calculate miles.
+    miles = km * CONVERSION_FACTOR
+
+    # Display the miles.
+    print(km, 'kilometers equals', format(miles, ',.3f'), 'miles.')
+
+# Call the main function.
+main()
