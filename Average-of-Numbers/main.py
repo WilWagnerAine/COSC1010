901f0caf575b54e7157bd5fe1766cfdd3fdbeb70
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-03-14 16:51:00 +0000

    Ready for Grading!

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,38 @@
 #
-# Name
-# Date
-# Average of Numbers Programming Project
+# Name: Wil Wagner
+# Date: 13 Mar 2025
+# Average of Numbers
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+# main function
+def main():
+    # Open the file number.txt.
+    with open('numbers.txt', 'r') as myfile:
+
+        # Declare variables
+        total = 0
+        lines = 0
+        number = 0
+
+        # Read the first line from the file.
+        line = myfile.readline()
+
+        # If the is not an empty string, continue.
+        while line != '':
+            # Add 1 to number of lines
+            lines += 1
+            # Place the number in line as interger in the number variable.
+            number = int(line)
+            # Add number variable to total variable
+            total += number
+            # Check for end of file for while loop.
+            line = myfile.readline()
+
+        # Calculate average.
+        average = total / lines
+        # Print average
+        print (f'The average of the numbers in the file is: {average}.')
+
+# Call main function
+main()
