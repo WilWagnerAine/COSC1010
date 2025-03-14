e9d51365f9ae84570ed04594431b8b5934c74c35
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-03-14 16:42:26 +0000

    Ready for Grading!

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
-# File Display Programming Project
+# Name: Wil Wagner
+# Date:
+# File Display
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+# main function
+def main():
+    # Open the file number.txt.
+    with open('numbers.txt', 'r') as myfile:
+
+        # Read the first line from the file.
+        line = myfile.readline()
+
+        # If the is not an empty string, continue.
+        while line != '':
+            # If converted to integer striping isn't necessary.
+            # Strip the '\n' from the end (right) of field.
+            line = line.rstrip('\n')
+            # Print the variable line.
+            print(line)
+            # Check for end of file for while loop.
+            line = myfile.readline()
+
+    # Do not need to close file while using with open.
+
+# Call main function
+main()
