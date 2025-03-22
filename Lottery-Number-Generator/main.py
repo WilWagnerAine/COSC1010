ef60fc07ec34e6fd044f53aaa64211f7c6605e7c
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-03-22 00:54:20 +0000

    Ready for Grading!

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,33 @@
 #
-# Name
-# Date
-# Lottery Number Generator Programming Project
+# Name: Wil Wagner
+# Date: 20 Mar 2025
+# Lottery Number Generator
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+# Import the random module
+import random
+
+# Define constants
+MAX_DIGITS = 7      # Max number of digits
+START = 0           # Start of random number range
+END = 9             # End of random number range
+
+# main function
+def main():
+
+    # Create list
+    numbers = []
+
+    # Append random number to list
+    for count in range(MAX_DIGITS):
+        numbers.append(random.randint(START, END))
+    
+    # Display lottery numbers
+    print('Here are your random lottery numbers:')
+    for count in range(MAX_DIGITS):
+        print(numbers[count], end=' ')
+    print()
+
+# Call main function
+main()
