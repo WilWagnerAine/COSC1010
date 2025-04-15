2219a0958337fdadac0cd5dee278858143ec92b8
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-04-15 22:53:18 +0000

    Ready for Grading!!!

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -5,6 +5,9 @@
 # COSC 1010
 #
 
+# Import pickle
+import pickle
+
 # Constant for the number of states to quiz the user on.
 NUM_QUEST = 5
 
@@ -54,31 +57,8 @@ def main():
 
 # Build a dictionary of states and capitals.
 def state_caps_dict():
-    sc = {'Alabama' : 'Montgomery', 'Alaska' : 'Juneau',
-          'Arizona' : 'Phoenix', 'Arkansas' : 'Little Rock',
-          'California' : 'Sacramento', 'Colorado' : 'Denver',
-          'Connecticut' : 'Hartford', 'Delaware' : 'Dover',
-          'Florida' : 'Tallahassee', 'Georgia' : 'Atlanta',
-          'Hawaii' : 'Honolulu', 'Idaho' : 'Boise',
-          'Illinois' : 'Springfield', 'Indiana' : 'Indianapolis',
-          'Iowa' : 'Des Moines', 'Kansas' : 'Topeka',
-          'Kentucky' : 'Frankfort', 'Louisiana' : 'Baton Rouge',
-          'Maine': 'Augusta', 'Maryland' : 'Annapolis',
-          'Massachusetts' : 'Boston', 'Michigan' : 'Lansing',
-          'Minnesota' : 'Saint Paul', 'Mississippi' : 'Jackson',
-          'Missouri' : 'Jefferson City', 'Montana' : 'Helena',
-          'Nebraska' : 'Lincoln', 'Nevada' : 'Carson City',
-          'New Hampshire' : 'Concord', 'New Jersey' : 'Trenton',
-          'New Mexico' : 'Santa Fe', 'New York' : 'Albany',
-          'North Carolina' : 'Raleigh', 'North Dakota' : 'Bismarck',
-          'Ohio' : 'Columbus', 'Oklahoma' : 'Oklahoma City',
-          'Oregon' : 'Salem', 'Pennsylvania' : 'Harrisburg',
-          'Rhode Island' : 'Providence', 'South Carolina' : 'Columbia',
-          'South Dakota' : 'Pierre', 'Tennessee' : 'Nashville',
-          'Texas' : 'Austin', 'Utah' : 'Salt Lake City',
-          'Vermont' : 'Montpelier', 'Virginia' : 'Richmond',
-          'Washington' : 'Olympia', 'West Virginia' : 'Charleston',
-          'Wisconsin' : 'Madison', 'Wyoming' : 'Cheyenne'}
+    with open('state_capital.dat', 'rb') as inputfile:
+        sc = pickle.load(inputfile)
 
     return sc
 
