29f4131e8a70bdf7480b307692da75ea03b145ba
WilWagnerAine <wilwagner@westernwyoming.edu>
2025-04-15 19:36:09 +0000

    Ready for Grading!

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,44 +1,87 @@
 #
-# Name
-# Date
-# Capital Quiz Programming Project
+# Name: Wil Wagner
+# Date: 15 April 2025
+# Capital Quiz Problem
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
 
-import random
+# Constant for the number of states to quiz the user on.
+NUM_QUEST = 5
 
+# main function
 def main():
-    # Initialize dictionary
-    capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
-                'Arizona':'Phoenix', 'Arkansas':'Little Rock',
-                'California':'Sacramento', 'Colorado':'Denver',
-                'Connecticut':'Hartford', 'Delaware':'Dover',
-                'Florida':'Tallahassee', 'Georgia':'Atlanta',
-                'Hawaii':'Honolulu', 'Idaho':'Boise',
-                'Illinois':'Springfield', 'Indiana':'Indianapolis',
-                'Iowa':'Des Moines', 'Kansas':'Topeka',
-                'Kentucky':'Frankfort', 'Louisiana':'Baton Rouge',
-                'Maine':'Augusta', 'Maryland':'Annapolis',
-                'Massachusetts':'Boston', 'Michigan':'Lansing',
-                'Minnesota':'Saint Paul', 'Mississippi':'Jackson',
-                'Missouri':'Jefferson City', 'Montana':'Helena',
-                'Nebraska':'Lincoln', 'Nevada':'Carson City',
-                'New Hampshire':'Concord', 'New Jersey':'Trenton',
-                'New Mexico':'Santa Fe', 'New York':'Albany',
-                'North Carolina':'Raleigh', 'North Dakota':'Bismarck',
-                'Ohio':'Columbus', 'Oklahoma':'Oklahoma City',
-                'Oregon':'Salem', 'Pennsylvania':'Harrisburg',
-                'Rhode Island':'Providence', 'South Carolina':'Columbia',
-                'South Dakota':'Pierre', 'Tennessee':'Nashville',
-                'Texas':'Austin', 'Utah':'Salt Lake City',
-                'Vermont':'Montpelier', 'Virginia':'Richmond',
-                'Washington':'Olympia', 'West Virginia':'Charleston',
-                'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
+    # Initialize the state_caps_dict.
+    state_caps = state_caps_dict()
 
-    # Local variables
+    # The repeat variable
+    again = 'y'
 
-    # Continue until user quits the game.
+    # Correct and incorrect variables
+    correct = 0
+    incorrect = 0
 
-# Call the main function.
-main()
+     # Continue to repeat quiz.
+    while again.lower() == 'y':
+
+        # Quiz the user.
+        for count in range(NUM_QUEST):
+            # Get a random entry from the dictionary.
+            state, capital = state_caps.popitem()
+
+            # Quiz the user.
+            print()
+            print('What is the capital of ', state, '? ', end='')
+            response = input()
+
+            # Check answer.
+            if response.lower() == capital.lower():
+                correct += 1
+                print('Correct!')
+            else:
+                incorrect += 1
+                print('Incorrect, sorry! The correct answer is: ', capital, '.')
+
+        # Results.
+        print()
+        print('Correct results:\t', correct)
+        print('Incorrect results:\t', incorrect)
+
+        # Ask if user wishes to take the quiz again.
+        print()
+        print('Would you like to take the quiz again?')
+        again = input('y for yes / any other for no: ')
+        print()
+
+# Build a dictionary of states and capitals.
+def state_caps_dict():
+    sc = {'Alabama' : 'Montgomery', 'Alaska' : 'Juneau',
+          'Arizona' : 'Phoenix', 'Arkansas' : 'Little Rock',
+          'California' : 'Sacramento', 'Colorado' : 'Denver',
+          'Connecticut' : 'Hartford', 'Delaware' : 'Dover',
+          'Florida' : 'Tallahassee', 'Georgia' : 'Atlanta',
+          'Hawaii' : 'Honolulu', 'Idaho' : 'Boise',
+          'Illinois' : 'Springfield', 'Indiana' : 'Indianapolis',
+          'Iowa' : 'Des Moines', 'Kansas' : 'Topeka',
+          'Kentucky' : 'Frankfort', 'Louisiana' : 'Baton Rouge',
+          'Maine': 'Augusta', 'Maryland' : 'Annapolis',
+          'Massachusetts' : 'Boston', 'Michigan' : 'Lansing',
+          'Minnesota' : 'Saint Paul', 'Mississippi' : 'Jackson',
+          'Missouri' : 'Jefferson City', 'Montana' : 'Helena',
+          'Nebraska' : 'Lincoln', 'Nevada' : 'Carson City',
+          'New Hampshire' : 'Concord', 'New Jersey' : 'Trenton',
+          'New Mexico' : 'Santa Fe', 'New York' : 'Albany',
+          'North Carolina' : 'Raleigh', 'North Dakota' : 'Bismarck',
+          'Ohio' : 'Columbus', 'Oklahoma' : 'Oklahoma City',
+          'Oregon' : 'Salem', 'Pennsylvania' : 'Harrisburg',
+          'Rhode Island' : 'Providence', 'South Carolina' : 'Columbia',
+          'South Dakota' : 'Pierre', 'Tennessee' : 'Nashville',
+          'Texas' : 'Austin', 'Utah' : 'Salt Lake City',
+          'Vermont' : 'Montpelier', 'Virginia' : 'Richmond',
+          'Washington' : 'Olympia', 'West Virginia' : 'Charleston',
+          'Wisconsin' : 'Madison', 'Wyoming' : 'Cheyenne'}
+
+    return sc
+
+#Call the main function.
+if  __name__  == '__main__':
+    main()
